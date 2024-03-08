1deaeaf2a19ea32f3132a02c967353b0a74e9457
shaun-saisi <saisishaun4@gmail.com>
2024-03-08 06:17:53 +0000

    Added all the classes that will be inheriting from the BaseModel

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The module is a Recview class
+"""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """For managing and reviewing objects
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
